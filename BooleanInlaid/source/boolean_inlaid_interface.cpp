ff0b719f16dc2e669895fc48b4f8e87fd6f2b3d3
moterech <14049998+moterech@users.noreply.github.com>
2020-08-15 18:40:30 +0900

    コメントを追加

--- BooleanInlaid/source/boolean_inlaid_interface.cpp
+++ BooleanInlaid/source/boolean_inlaid_interface.cpp
@@ -8,6 +8,7 @@ bool boolean_inlaid_interface::do_browser(sxsdk::scene_interface* scene, void*) 
         scene->message("Aborted. There are too many shapes.");
         return false;
     }
+    // =(表面材質を置き換える) が設定された形状を抽出
     sxsdk::shape_class* base_shapes[MAX_SHAPES] = { nullptr };
     sxsdk::shape_class* inlaying_shapes[MAX_SHAPES] = { nullptr };
     for (size_t i = 0; i < len; i++)
@@ -21,10 +22,12 @@ bool boolean_inlaid_interface::do_browser(sxsdk::scene_interface* scene, void*) 
             base_shapes[i] = source_shapes[i];
         }
     }
+    // ブーリアン演算による置き換え処理を実行
     for (size_t i = 0; i < len; i++)
     {
         if (base_shapes[i] != nullptr) {
             sxsdk::shape_class* tmp_shapes[MAX_SHAPES] = { nullptr };
+            // 置き換える形状との交差部分をくり抜いて穴をあける
             scene->select_shapes(1, &base_shapes[i]);
             scene->begin_boolean_shapes();
             scene->add_boolean_shape(base_shapes[i]);
@@ -37,6 +40,7 @@ bool boolean_inlaid_interface::do_browser(sxsdk::scene_interface* scene, void*) 
             scene->end_boolean_shapes();
             scene->do_manifold_boolean(sxsdk::smlib::boolean_operation_type::bo_surface_different, -1, -1, sxsdk::enums::surface_replicator_subdivision_level::surface_replicator_subdivision_regular, true, true);
             scene->get_active_shapes(&tmp_shapes[i]);
+            // 置き換える形状との交差部分を表面として切り抜き
             for (size_t j = 0; j < len; j++)
             {
                 if (inlaying_shapes[j] != nullptr) {
@@ -47,6 +51,7 @@ bool boolean_inlaid_interface::do_browser(sxsdk::scene_interface* scene, void*) 
                     scene->end_boolean_shapes();
                     scene->do_manifold_boolean(sxsdk::smlib::boolean_operation_type::bo_surface_intersection, -1, -1, sxsdk::enums::surface_replicator_subdivision_level::surface_replicator_subdivision_regular, true, true);
                     scene->get_active_shapes(&tmp_shapes[j]);
+                    // 切り抜いた表面の材質を設定
                     sxsdk::shape_class *surface = tmp_shapes[j]->get_son()->get_bro();
                     sxsdk::polygon_mesh_class& mesh = surface->get_polygon_mesh();
                     for (size_t k = 0;  k < mesh.get_number_of_face_groups();  k++)
@@ -55,6 +60,7 @@ bool boolean_inlaid_interface::do_browser(sxsdk::scene_interface* scene, void*) 
                     }
                 }
             }
+            // くり抜いた形状と表面を合体
             scene->begin_boolean_shapes();
             for (size_t j = 0; j < len; j++)
             {
@@ -64,6 +70,7 @@ bool boolean_inlaid_interface::do_browser(sxsdk::scene_interface* scene, void*) 
             }
             scene->end_boolean_shapes();
             scene->do_manifold_boolean(sxsdk::smlib::boolean_operation_type::bo_merge, -1, -1, sxsdk::enums::surface_replicator_subdivision_level::surface_replicator_subdivision_no_subdivision, false, false);
+            // 不要な形状を削除
             for (size_t j = 0; j < len; j++)
             {
                 if (tmp_shapes[j] != nullptr) {
